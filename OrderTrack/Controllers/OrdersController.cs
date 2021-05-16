7d6210984a918ad6b0e23411f04cac8997122a4b
Tyler Sinks <tyler.sinks@gmail.com>
2021-05-16 10:20:52 -0400

    add index, new, and show routes

---
+++ OrderTrack/Controllers/OrdersController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using OrderTrack.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTrack.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/orders")]
+    public ActionResult Index(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      List<Order> _allOrders = Order.GetAllOrders();
+      Dictionary<string, object> model = new Dictionary<string, object> {"vendor", };
+      return View();
+    }
+
+    [HttpGet("/orders/new")]
+    public ActionResult CreateOrder(int vendorId)
+    {
+      Vendor model = Vendor.Find(vendorId);
+      return View(model);
+    }
+
+    [HttpPost("/orders")]
+    public ActionResult CreateOrder(string title, string description, string baguettes, string croissants, Vendor vendor)
+    {
+      DateTime date = DateTime.Now;
+      Order newOrder = new Order(title, description, date);
+      newOrder.CalcPrice(baguettes, croissants);
+      Vendor model = Vendor.Find(vendor.VendorId);
+      int id = model.VendorId;
+      return RedirectToAction("Index", id);
+    }
+  }
+}
